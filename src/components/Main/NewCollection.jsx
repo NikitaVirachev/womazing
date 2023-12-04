d62ff72a691050a18dffd32de0626645916c0039
NikitaVirachev <guardsolder@gmail.com>
2023-12-04 17:19:16 +0300

    feat: start implement NewCollection container

--- src/components/Main/NewCollection.jsx
+++ src/components/Main/NewCollection.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+import ButtonCTA from '../ButtonCTA.jsx';
+
+const NewCollectionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6.5rem;
+`;
+
+const Goods = styled.div`
+  display: flex;
+  gap: 3rem;
+`;
+
+const ButtonCTAContainer = styled.div`
+  text-align: center;
+`;
 
 const NewCollection = () => {
-  return <div>New Collection</div>;
+  const navigate = useNavigate();
+
+  const btnClickHandler = () => {
+    navigate('/store');
+  };
+
+  return (
+    <NewCollectionContainer>
+      <Goods></Goods>
+      <ButtonCTAContainer>
+        <ButtonCTA $isOutline={true} onClick={btnClickHandler}>
+          Открыть магазин
+        </ButtonCTA>
+      </ButtonCTAContainer>
+    </NewCollectionContainer>
+  );
 };
 
 export default NewCollection;
